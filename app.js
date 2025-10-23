24a70ddf2101fb5be0cc89126f6d88b352ef892e
Ramon Dias <ramondias.profissional@gmail.com>
2025-10-22 21:26:15 -0300

    Adiciona arquivos e atualiza app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ while (chute != numeroSecreto) {
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu qual é o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
 // if (tentativas > 1) {
 //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
